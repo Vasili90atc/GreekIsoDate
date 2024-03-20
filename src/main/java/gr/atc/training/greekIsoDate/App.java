d3e481bfab5a2cd4c4fc757ec0937caf2c1a3d13
Ioanna-Nikoletta Vasili <IVasili@atc.gr>
2024-03-20 12:43:52 +0200

    commit

--- src/main/java/gr/atc/training/greekIsoDate/App.java
+++ src/main/java/gr/atc/training/greekIsoDate/App.java
@@ -4,9 +4,11 @@ public class App {
 	
     public static void main( String[] args ) {
        DateProcessor dp = new DateProcessor();
-       dp.printGreekDate("19901231T235959Z");
-       dp.printGreekDate("19980221T154545Z");
-       dp.printGreekDate("20240314T064101Z");
-       dp.printGreekDate("19520802T120555Z");
+       //dp.printGreekDate("19901231T235959Z");
+       //dp.printGreekDate("19980221T154545Z");
+       //dp.printGreekDate("20240314T064101Z");
+       //dp.printGreekDate("19520802T120555Z");
+       //System.out.println("\n");
+       dp.printGreekDifferenceDate("20241201T115929Z", "20240221T220029Z");
     }
 }
--- src/main/java/gr/atc/training/greekIsoDate/DateProcessor.java
+++ src/main/java/gr/atc/training/greekIsoDate/DateProcessor.java
@@ -1,7 +1,11 @@
 package gr.atc.training.greekIsoDate;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 
 public class DateProcessor {
@@ -13,5 +17,38 @@ public class DateProcessor {
 		
 		System.out.println(greekDate);
 		return isoDate;
+	}
+	
+	public String printGreekDifferenceDate(String isoDate1, String isoDate2) {
+		LocalDateTime date1 = LocalDateTime.parse(isoDate1, DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'"));
+		LocalDateTime date2 = LocalDateTime.parse(isoDate2, DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'"));
+		LocalDateTime result = date1.minusYears(date2.getYear());
+		result = result.minusDays(date2.getDayOfYear());
+		result = result.minusHours(date2.getHour());
+		result = result.minusMinutes(date2.getMinute());
+		result = result.minusSeconds(date2.getSecond());
+		
+		
+		int days = result.getDayOfYear() + result.getYear()*365;
+		String hours_str = String.valueOf(result.getHour());
+		String minutes_str = String.valueOf(result.getMinute());
+		String seconds_str = String.valueOf(result.getSecond());
+		
+		if (result.getHour() == 0)hours_str = "00";
+		if (result.getMinute() == 0) minutes_str = "00";
+		if (result.getSecond() == 0) seconds_str = "00";
+		
+		String answer = String.valueOf(days) + " μέρες " + hours_str + ":" + minutes_str + ":" + seconds_str;
+		
+		System.out.println(answer);
+		return answer;
+		
+		/* 
+		 * 
+		 * DateTimeFormatter fmt = DateTimeFormatter.ofPattern("D μέρες H:m:s");
+		String diffDate = result.format(fmt);
+		System.out.println(diffDate);
+		*/
+		
 	}
 }
